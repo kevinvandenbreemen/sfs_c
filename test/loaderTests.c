5845a89a4186d22174002da9a650b72b5044a508
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2020-05-02 18:08:29 -0400

    Validate that system can detect file that is not an SFS

--- test/loaderTests.c
+++ test/loaderTests.c
@@ -2,7 +2,12 @@
 #include "../src/sfs_c.h"
 
 START_TEST(LoadAFile) {
-    fail_if(sfs_checkIsSFS("./resource/exampleSFS") != 1, "Example SFS should have been recognized as an SFS file");
+    fail_unless(sfs_checkIsSFS("./resource/exampleSFS") == 1, "Example SFS should have been recognized as an SFS file");
+}
+END_TEST
+
+START_TEST(NOT_AN_SFS) {
+    fail_if(sfs_checkIsSFS("./resource/nonSFS") == 1, "File that is not an SFS file should not be recognized as SFS file");
 }
 END_TEST
 
@@ -14,6 +19,7 @@ int main(void)
 
     TCase *case1 = tcase_create("Basic Open");
     tcase_add_test(case1, LoadAFile);
+    tcase_add_test(case1, NOT_AN_SFS);
     suite_add_tcase(suite, case1);
 
     srunner_run_all(runner, CK_ENV);
