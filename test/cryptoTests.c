abe0bc2d906d2ad3b2c8eae80df643d58cfb2d2b
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2020-05-13 18:20:57 -0400

    Create a sample of encrypting using AES 256

--- test/cryptoTests.c
+++ test/cryptoTests.c
@@ -23,14 +23,47 @@ START_TEST(OpenCipherHandle) {
 }
 END_TEST
 
-START_TEST(SetCipherKey) {
+START_TEST(DoAESEncrypt) {
+
+    //  Based on code found here
+    //  https://cboard.cprogramming.com/c-programming/105743-how-decrypt-encrypt-using-libgcrypt-arc4.html#post937372
+    gcry_error_t error;
+
+    char * txtBuffer = "123456789 abcdefghijklmnopqrstuvwzyz ABCDEFGHIJKLMNOPQRSTUVWZYZ";
+    size_t txtLength = strlen(txtBuffer)+1; // string plus termination
+    char * encBuffer = malloc(txtLength);
+    char * outBuffer = malloc(txtLength);
 
     //  See also https://gnupg.org/documentation/manuals/gcrypt/Working-with-cipher-handles.html#Working-with-cipher-handles
     gcry_cipher_hd_t handle;
-    gcry_cipher_open(&handle, GCRY_CIPHER_AES256, GCRY_CIPHER_MODE_CBC, GCRY_CIPHER_SECURE);
+    error = gcry_cipher_open(&handle, GCRY_CIPHER_AES256, GCRY_CIPHER_MODE_CBC, GCRY_CIPHER_SECURE);
+    fail_if(error);
 
-    char key[32];
-    printf("Result of keyset:  %d\n", gcry_cipher_setkey(handle, &key, 32));
+    size_t keyLength = gcry_cipher_get_algo_keylen(GCRY_CIPHER_AES256);
+    char key[32] = "one test AES keyone test AES key";
+
+    error = gcry_cipher_setkey(handle, key, keyLength);
+    fail_if(error);
+
+    printf("Result of keyset:  %d\n", error);
+
+    char *iv = "a test ini value";
+
+    error = gcry_cipher_setiv(handle, iv, 16);
+    fail_if(error);
+
+    error = gcry_cipher_encrypt(handle, encBuffer, txtLength, txtBuffer, txtLength);
+    fail_if(error);
+
+    //  TODO    Why do we need this???
+    error = gcry_cipher_setiv(handle, iv, 16);
+    fail_if(error);
+
+    error = gcry_cipher_decrypt(handle, outBuffer, txtLength, encBuffer, txtLength);
+    fail_if(error);
+
+    printf("OutBuff:  %s\n", outBuffer);
+    fail_if(memcmp(outBuffer, txtBuffer, txtLength) != 0);
 
     gcry_cipher_close(handle);
 
@@ -45,7 +78,7 @@ int main(int argc, char const *argv[])
     TCase *case1 = tcase_create("Cipher Fiddling Around");
 
     tcase_add_test(case1, OpenCipherHandle);
-    tcase_add_test(case1, SetCipherKey);
+    tcase_add_test(case1, DoAESEncrypt);
     suite_add_tcase(suite, case1);
 
     srunner_run_all(runner, CK_ENV);
