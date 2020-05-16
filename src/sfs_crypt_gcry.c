7c7cf209390f92fdd6a96db29d7d8667b3c13202
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2020-05-16 17:05:21 -0400

    Code cleanup

--- src/sfs_crypt_gcry.c
+++ src/sfs_crypt_gcry.c
@@ -11,32 +11,22 @@ char * sfs_encrypt(char *data, char *password, int length){
     //  Based on code found here
     //  https://cboard.cprogramming.com/c-programming/105743-how-decrypt-encrypt-using-libgcrypt-arc4.html#post937372
     gcry_error_t error;
-
     char * txtBuffer = data;
-
     char * encBuffer = malloc(length);
 
     //  See also https://gnupg.org/documentation/manuals/gcrypt/Working-with-cipher-handles.html#Working-with-cipher-handles
     gcry_cipher_hd_t handle;
     error = gcry_cipher_open(&handle, GCRY_CIPHER_AES256, GCRY_CIPHER_MODE_CBC, GCRY_CIPHER_SECURE);
-    
-
     size_t keyLength = gcry_cipher_get_algo_keylen(GCRY_CIPHER_AES256);
-
     error = gcry_cipher_setkey(handle, password, keyLength);
-    
-
-    printf("Result of keyset:  %d\n", error);
 
     char *iv = "a test ini value";
 
     error = gcry_cipher_setiv(handle, iv, 16);
-    
     error = gcry_cipher_encrypt(handle, encBuffer, length, txtBuffer, length);
 
     char *finalProduct = malloc(length + IV_LEN);
     memcpy(&finalProduct[0], iv, IV_LEN);
-    printf("Will write %d to final product at %d\n", length, IV_LEN);
     memcpy(&finalProduct[IV_LEN], &encBuffer[0], length);
     
     gcry_cipher_close(handle);
